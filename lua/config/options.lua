fa26b98f3ce396942f53733b856ce1716595ca60
khietbt <khietbt@gmail.com>
2024-03-04 14:01:09 +0700

    Update nvim following devaslife

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.colorcolumn = "120"
+vim.opt.conceallevel = 0
 vim.opt.listchars = "tab:-,trail:•,precedes:←,extends:→,eol:↲,nbsp:␣,space:␣"
 vim.opt.relativenumber = false
 vim.opt.showbreak = "↪\\ "
-vim.opt.conceallevel = 0
---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 15000,
+    },
+  },
+}
